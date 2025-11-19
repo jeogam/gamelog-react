44e07ad6bb31d16ff7e4c3a393dd1b7166215630
JeoGam <jeovanigamati@gmail.com>
2025-11-19 11:04:34 -0300

    feat(footer): adicionar componente footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-// Importando a Navbar
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
-// Páginas
 import Landing from './pages/Landing'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Sandbox from './pages/Sandbox'
+import Hades from './pages/Hades' 
 
 function App() {
   return (
     <BrowserRouter>
-      {/* A Navbar fica FORA do Routes para aparecer sempre */}
       <Navbar />
 
       <div className="container">
@@ -21,7 +20,12 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/sandbox" element={<Sandbox />} />
+          
+          {/* 2. Adicione a rota aqui */}
+          <Route path="/hades" element={<Hades />} /> 
         </Routes>
+        
+        <Footer /> 
       </div>
     </BrowserRouter>
   )
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2025 GameLog. Todos os direitos reservados.</p>
+    </footer>
+  )
+}
+
+export default Footer
