7195f1f49a3212d9549ed1f09b610c04c971f939
JeoGam <jeovanigamati@gmail.com>
2025-11-19 10:41:58 -0300

    feat(react-router): Adicionando React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+// 1. Importar o CSS do Bootstrap (instalado via npm)
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// 2. Importar o JS do Bootstrap (para o menu mobile e modais funcionarem)
+// @ts-ignore
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+// 3. Importar o teu CSS personalizado (o caminho deve bater com a pasta que criamos)
+import './assets/css/style.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
